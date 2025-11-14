bfc8824b6a68ec6d651a368baf8b4c8654a513a6
Aditi Saxena <157038896+aditisaxena259@users.noreply.github.com>
2025-11-14 11:03:54 +0530

    Add API endpoint constants in app.ts
    Define constants for various API endpoints using environment variables.

---
+++ next-beauty/app.ts
@@ -0,0 +1,20 @@
+export const API_PRODUCTS =
+  process.env.REACT_APP_API_PRODUCTS as string;
+
+export const API_PROFILE =
+  process.env.REACT_APP_API_PROFILE as string;
+
+export const API_PROFILE_UPDATE =
+  process.env.REACT_APP_API_PROFILE_UPDATE as string;
+
+export const API_INVENTORY =
+  process.env.REACT_APP_API_INVENTORY as string;
+
+export const API_INVENTORY_ADD =
+  process.env.REACT_APP_API_INVENTORY_ADD as string;
+
+export const API_INVENTORY_UPDATE =
+  process.env.REACT_APP_API_INVENTORY_UPDATE as string;
+
+export const API_INVENTORY_DELETE =
+  process.env.REACT_APP_API_INVENTORY_DELETE as string;
